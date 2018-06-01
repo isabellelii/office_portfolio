35824ecdc110d88a6f761d6bc1dbfcfd2d743c46
isabelle <isabellelidberg@live.se>
2018-06-01 15:04:05 +0200

    displays building data in table installs rxjs-compat to fix observable error

--- src/app/display-buildings.service.ts
+++ src/app/display-buildings.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
+import { BUILDINGS } from './mock-data';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,12 @@ import { Injectable } from '@angular/core';
 export class DisplayBuildingsService {
 
   constructor() { }
+
+  getBuildings(): Observable<any[]>{
+    return Observable.of(BUILDINGS).delay(100);
+  }
+
+  getColumns(): string[]{
+    return ["building", "size", "usage", "capacity"]
+  };
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayBuildingsService } from '../display-buildings.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  templateUrl: './table.component.html'
 })
 export class TableComponent implements OnInit {
+  buildings: Observable<any[]>;
+  columns: string[];
 
-  constructor() { }
+  constructor(private atService: DisplayBuildingsService) {}
 
   ngOnInit() {
+    this.columns = this.atService.getColumns();
+    this.buildings = this.atService.getBuildings();
   }
 
 }
