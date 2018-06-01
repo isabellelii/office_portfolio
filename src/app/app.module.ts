fb6890671606ec393f7b84db969a8bf54788de18
isabelle <isabellelidberg@live.se>
2018-06-01 15:14:50 +0200

    creates map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
