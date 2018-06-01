e1c6511039f195b8b2df6dd22f89e27e8cf76c46
isabelle <isabellelidberg@live.se>
2018-06-01 14:36:36 +0200

    create fake-backend with json attributes  for buildings

---
+++ src/app/mock-data.ts
@@ -0,0 +1,63 @@
+export const BUILDINGS: any[] =
+[
+  {
+    building: "OfficeLab",
+    size: 213,
+    usage: "35%",
+    capacity: 139
+  },
+  {
+    building: "Prime Tower",
+    size: 380,
+    usage: "61%",
+    capacity: 147
+  },
+  {
+    building: "Toranomon Hills",
+    size: 1380,
+    usage: "42%",
+    capacity: 819
+  },
+  {
+    building: "Kohinoor Square",
+    size: 400,
+    usage: "79%",
+    capacity: 83
+  },
+  {
+    building: "Edifício Itália",
+    size: 700,
+    usage: "94%",
+    capacity: 41
+  },
+  {
+    building: "Shard London Bridge",
+    size: 400,
+    usage: "53%",
+    capacity: 187
+  },
+  {
+    building: "One Canada Square",
+    size: 1607,
+    usage: "45%",
+    capacity: 890
+  },
+  {
+    building: "110 Bishopsgate",
+    size: 2013,
+    usage: "43%",
+    capacity: 1152
+  },
+  {
+    building: "Leadenhall",
+    size: 360,
+    usage: "60%",
+    capacity: 144
+  },
+  {
+    building: "Lodha Bellezza",
+    size: 1360,
+    usage: "29%",
+    capacity: 968
+  },
+]
